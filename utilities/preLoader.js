c4c9aa3cae3f28c0103d5ebc22041486e6b40854
mua-wallace <muawallizewi@gmail.com>
2021-12-07 11:18:20 +0100

    refactored to preloader.css

---
+++ utilities/preLoader.js
@@ -0,0 +1,12 @@
+
+// add preLoader
+function addPreload() {
+    $('.preload').attr('id','preloader');
+  } 
+
+//   remove preLoader
+  function removePreload() {
+    $('.preload').removeAttr('id');
+  } 
+
+  
